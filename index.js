c1fa82753674ade4f30ba77d4f273d3d5e5758fe
olafurrg <olafurg16@ru.is>
2021-04-26 23:31:01 +0000

    scrape through all relevant data

--- index.js
+++ index.js
@@ -8,6 +8,48 @@ async function scrapeSite(url){
 
         await page.waitForXPath('/html/body/div/div/div/div[3]/div[1]/div/div[2]/img');
         await page.click('#root > div > div > div:nth-child(3) > div:nth-child(1) > button');
+        
+        const outerDiv = await page.$$('.sc-knSEWW');
+        console.log(outerDiv.length);
+        let increment = 1;
+        for (const innerDiv of outerDiv){
+            
+            /*
+            const name = await innerDiv.$eval('span', (span) => {
+                return span.innerText
+            });
+            const photo = await innerDiv.$eval('img', (img) => {
+                return img.src
+            });
+            console.log(name, photo);
+            */
+
+            //Get photo link
+            const [el] = await page.$x(`/html/body/div/div/div/div[${increment}]/div[1]/div/div[2]/img`);
+            const src = await el.getProperty('src');
+            const oponentPhoto = await src.jsonValue();
+            //Get oponent name
+            const [el2] = await page.$x(`//*[@id="root"]/div/div/div[${increment}]/div[1]/div/div[2]/div/div[1]/span[1]`);
+            const txt = await el2.getProperty('textContent');
+            const oponent = await txt.jsonValue();
+            //Get Time
+            const [el3] = await page.$x(`//*[@id="root"]/div/div/div[${increment}]/div[1]/div/div[2]/div/div[2]/span[1]`);
+            const txt2 = await el3.getProperty('textContent');
+            const time = await txt2.jsonValue();
+            //Get day
+            const [el4] = await page.$x(`//*[@id="root"]/div/div/div[${increment}]/div[1]/div/div[2]/div/div[2]/span[2]`);
+            const txt3 = await el4.getProperty('textContent');
+            const day = await txt3.jsonValue();
+
+            increment++;
+
+
+        }
+
+        
+        
+        
+        /*
         //Get photo link
         const [el] = await page.$x('/html/body/div/div/div/div[3]/div[1]/div/div[2]/img');
         const src = await el.getProperty('src');
@@ -27,6 +69,7 @@ async function scrapeSite(url){
 
         console.log({oponentPhoto, oponent, time, day}); 
         return { oponentPhoto, oponent, time, day }
+        */
     } catch(err){
         console.error(err.message);
     } finally {
